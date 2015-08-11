bfcaceb2fd59d17bbe1909653dabc60ac34b1dc9
Nikita Semakov <n.semakov@yahoo.com>
2015-08-11 03:57:47 +0300

    Initial Commit

---
+++ 30. DynamicCells/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  30. DynamicCells
+//
+//  Created by Admin on 11.08.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
