80acca16c4ac7d8e3dc7b6773ecd0647101c9d0b
Nikita Semakov <n.semakov@yahoo.com>
2015-08-11 04:40:22 +0300

    Uchenik

--- 30. DynamicCells/ViewController.m
+++ 30. DynamicCells/ViewController.m
@@ -17,11 +17,50 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    //self.tableView.delegate=self;
+    self.tableView.dataSource=self;
+    UIEdgeInsets inset=UIEdgeInsetsMake(20, 0, 0, 0);
+    self.tableView.scrollIndicatorInsets=inset;
+    self.tableView.contentInset=inset;
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+#pragma mark - UITableViewDataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 1000;
+}
 
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    NSString *identifier=@"cell";
+    UITableViewCell *cell=[tableView dequeueReusableCellWithIdentifier:identifier];
+    if (!cell) {
+        cell=[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
+    }
+    UIColor *cellColor=[self randomColor];
+    CGFloat redColorComponent;
+    CGFloat greenColorComponent;
+    CGFloat blueColorComponent;
+    CGFloat alphaColorComponent;
+    
+    [cellColor getRed:&redColorComponent green:&greenColorComponent blue:&blueColorComponent alpha:&alphaColorComponent];
+    cell.backgroundColor=cellColor;
+    cell.textLabel.text=[NSString stringWithFormat:@"RGB(%.0f,%.0f,%.0f)",roundf(redColorComponent*255.f) ,roundf(greenColorComponent*255.f),roundf(blueColorComponent*255.f)];
+    
+    return cell;
+    
+}
+
+
+#pragma mark - hepl methods
+- (UIColor*) randomColor {
+    CGFloat redColorComponent=(float)arc4random_uniform(256)/255.f;
+    CGFloat greenColorComponent=(float)arc4random_uniform(256)/255.f;
+    CGFloat blueColorComponent=(float)arc4random_uniform(256)/255.f;
+    UIColor *color=[UIColor colorWithRed:redColorComponent green:greenColorComponent blue:blueColorComponent alpha:1.f];
+    return color;
+}
 @end
